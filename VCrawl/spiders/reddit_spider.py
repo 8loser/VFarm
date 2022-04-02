24da74c3e6832339b4f8da7e3d03f6e508a2473f
8loser <8loser@gmail.com>
2022-04-02 23:47:33 +0800

    add praw

--- VCrawl/spiders/reddit_spider.py
+++ VCrawl/spiders/reddit_spider.py
@@ -1,10 +1,14 @@
 from scrapy import Spider
+import praw
 
 
 class RedditSpider(Spider):
     name = 'reddit'
-    allowed_domains = ['reddit.com']
-    start_urls = ['https://www.reddit.com/r/funnyvideos/']
+    # allowed_domains = ['reddit.com']
+    # start_urls = ['https://www.reddit.com/r/funnyvideos/']
+    reddit = praw.Reddit("bot1", user_agent="script bot agent")
+    for item in reddit.user.me().saved(limit=None):
+        print(item)
 
 
 def parse(self, response):
